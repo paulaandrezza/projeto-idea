b80d3ac3bea7e9cee0ee3afe9714158d9ce48757
paulaandrezza <paulaandrezza25@gmail.com>
2023-02-18 14:32:57 -0300

    Firts commit

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+const differentialsInfo = [
+  {
+    title: 'atendimento',
+    info: 'Prezamos pela satisfação dos nossos clientes, dando toda assistência necessária.',
+  },
+  {
+    title: 'valores',
+    info: 'Prestamos serviços de qualidade, com valores que cabem no seu bolso.',
+  },
+  {
+    title: 'qualidade',
+    info: 'A confiança no nosso trabalho é estabelecida através dos serviços de qualidade que prestamos aos nosssos clientes.',
+  },
+  {
+    title: 'pontualidade',
+    info: 'Prezamos por um bom serviço entregue com pontualidade e responsabilidade.',
+  },
+]
+
+
+const differentials = document.getElementById('differentials-list');
+
+
+function convertArrayToLi(differentials) {
+  return `
+    <li>
+      <img src="assets/img/${differentials.title}.png" alt="icone de ${differentials.title}">
+      <h3>${differentials.title}</h3>
+      <p>${differentials.info}</p>
+    </li>
+  `
+}
+
+
+
+function createHtmlDifferentials() {
+  const newHtml = differentialsInfo.map(convertArrayToLi).join('')
+  differentials.innerHTML = newHtml
+}
+
+createHtmlDifferentials();
